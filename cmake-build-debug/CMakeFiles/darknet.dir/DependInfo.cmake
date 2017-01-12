05b199c70b29ed8c37a4f6ec7c83fa56bd4b6ca1
hustzxd <2227820226@qq.com>
2017-01-12 22:38:31 +0800

    debug darknet in Clion

---
+++ cmake-build-debug/CMakeFiles/darknet.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/zxd/projects/darknet-master/src/darknet.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/darknet.dir/src/darknet.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GPU"
+  "OPENCV"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/usr/include/opencv"
+  "/usr/include/python2.7"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/dnl.dir/DependInfo.cmake
@@ -0,0 +1,87 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/zxd/projects/darknet-master/src/activation_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/activation_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/activations.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/activations.c.o"
+  "/home/zxd/projects/darknet-master/src/art.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/art.c.o"
+  "/home/zxd/projects/darknet-master/src/avgpool_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/avgpool_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/batchnorm_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/batchnorm_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/blas.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/blas.c.o"
+  "/home/zxd/projects/darknet-master/src/box.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/box.c.o"
+  "/home/zxd/projects/darknet-master/src/captcha.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/captcha.c.o"
+  "/home/zxd/projects/darknet-master/src/cifar.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/cifar.c.o"
+  "/home/zxd/projects/darknet-master/src/classifier.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/classifier.c.o"
+  "/home/zxd/projects/darknet-master/src/coco.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/coco.c.o"
+  "/home/zxd/projects/darknet-master/src/col2im.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/col2im.c.o"
+  "/home/zxd/projects/darknet-master/src/compare.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/compare.c.o"
+  "/home/zxd/projects/darknet-master/src/connected_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/connected_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/convolutional_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/convolutional_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/cost_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/cost_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/crnn_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/crnn_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/crop_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/crop_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/cuda.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/cuda.c.o"
+  "/home/zxd/projects/darknet-master/src/darknet.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/darknet.c.o"
+  "/home/zxd/projects/darknet-master/src/data.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/data.c.o"
+  "/home/zxd/projects/darknet-master/src/deconvolutional_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/deconvolutional_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/demo.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/demo.c.o"
+  "/home/zxd/projects/darknet-master/src/detection_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/detection_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/detector.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/detector.c.o"
+  "/home/zxd/projects/darknet-master/src/dice.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/dice.c.o"
+  "/home/zxd/projects/darknet-master/src/dropout_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/dropout_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/gemm.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/gemm.c.o"
+  "/home/zxd/projects/darknet-master/src/go.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/go.c.o"
+  "/home/zxd/projects/darknet-master/src/gru_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/gru_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/im2col.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/im2col.c.o"
+  "/home/zxd/projects/darknet-master/src/image.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/image.c.o"
+  "/home/zxd/projects/darknet-master/src/layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/layer.c.o"
+  "/home/zxd/projects/darknet-master/src/list.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/list.c.o"
+  "/home/zxd/projects/darknet-master/src/local_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/local_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/matrix.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/matrix.c.o"
+  "/home/zxd/projects/darknet-master/src/maxpool_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/maxpool_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/network.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/network.c.o"
+  "/home/zxd/projects/darknet-master/src/nightmare.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/nightmare.c.o"
+  "/home/zxd/projects/darknet-master/src/normalization_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/normalization_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/option_list.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/option_list.c.o"
+  "/home/zxd/projects/darknet-master/src/parser.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/parser.c.o"
+  "/home/zxd/projects/darknet-master/src/region_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/region_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/reorg_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/reorg_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/rnn.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/rnn.c.o"
+  "/home/zxd/projects/darknet-master/src/rnn_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/rnn_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/rnn_vid.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/rnn_vid.c.o"
+  "/home/zxd/projects/darknet-master/src/route_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/route_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/shortcut_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/shortcut_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/softmax_layer.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/softmax_layer.c.o"
+  "/home/zxd/projects/darknet-master/src/super.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/super.c.o"
+  "/home/zxd/projects/darknet-master/src/swag.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/swag.c.o"
+  "/home/zxd/projects/darknet-master/src/tag.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/tag.c.o"
+  "/home/zxd/projects/darknet-master/src/tree.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/tree.c.o"
+  "/home/zxd/projects/darknet-master/src/utils.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/utils.c.o"
+  "/home/zxd/projects/darknet-master/src/voxel.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/voxel.c.o"
+  "/home/zxd/projects/darknet-master/src/writing.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/writing.c.o"
+  "/home/zxd/projects/darknet-master/src/yolo.c" "/home/zxd/projects/darknet-master/cmake-build-debug/CMakeFiles/dnl.dir/src/yolo.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GPU"
+  "OPENCV"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/usr/include/opencv"
+  "/usr/include/python2.7"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
